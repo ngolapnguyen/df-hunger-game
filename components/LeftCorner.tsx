1e0af8f4f56bba66ca8940dee85b50cbd49008a3
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2022-12-01 15:31:36 +0700

    feat: update

--- components/LeftCorner.tsx
+++ components/LeftCorner.tsx
@@ -6,7 +6,12 @@ const RoundCountdown = () => {
   const { gameState } = useGameContext();
   const { seconds } = useCountdown(new Date(gameState!.round_expire_at));
 
-  return <>{seconds}s</>;
+  return (
+    <>
+      {seconds}
+      <small>s</small>
+    </>
+  );
 };
 
 export const LeftCorner = () => {
@@ -14,28 +19,12 @@ export const LeftCorner = () => {
 
   return (
     <div className="left-corner">
-      {currentPlayer && (
-        <div className="points">Points: {currentPlayer?.points || 0}</div>
-      )}
-      <div className="items">
-        <span>
-          <span className="item point-2" />: 2
-        </span>
-        <span>
-          <span className="item point-4" />: 4
-        </span>
-        <span>
-          <span className="item point-8" />: 8
-        </span>
-        <span>
-          <span className="item point-16" />: 16
-        </span>
-      </div>
-      <div>Game ID: {gameState?.id}</div>
-      <div>Game Status: {gameState?.status}</div>
       <div className="rounds">
         <span>
-          Next round in: {gameState?.round_expire_at ? <RoundCountdown /> : "-"}
+          Next round in:{" "}
+          <span className="countdown">
+            {gameState?.round_expire_at ? <RoundCountdown /> : "-"}
+          </span>
         </span>
         {isLoadingGameState && (
           <Image
@@ -47,6 +36,26 @@ export const LeftCorner = () => {
           />
         )}
       </div>
+      {currentPlayer && (
+        <div className="points">Points: {currentPlayer?.points || 0}</div>
+      )}
+      <div className="items">
+        <span>
+          <span className="item point-2" />: 2
+        </span>
+        <span>
+          <span className="item point-4" />: 4
+        </span>
+        <span>
+          <span className="item point-8" />: 6
+        </span>
+        <span>
+          <span className="item point-16" />: 8
+        </span>
+      </div>
+
+      <div>Game Status: {gameState?.status}</div>
+      <div>Game ID: {gameState?.id}</div>
     </div>
   );
 };
