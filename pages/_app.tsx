bc4e3e3e5dfeca7fa324309df0aa847f7d373d9d
Toan Ho <toanbku@gmail.com>
2023-02-13 16:49:28 +0700

    chore: update meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,22 +5,21 @@ import Head from "next/head";
 import { GameContextProvider } from "../contexts/game";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const metaDesc =
+    "Join the Hunger Game fun with four players and a random portal location. Collect points while racing to find the portal before your opponents. Just watch out for explosions!";
   return (
     <>
       <Head>
         <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <title>Hunger Game | Dwarves Foundation</title>
-        <meta property="og:title" content="Hunger Game | Dwarves Foundation" />
+        <title>Hunger Game</title>
+        <meta property="og:title" content="Hunger Game" />
         <meta name="twitter:site" content="@dwarvesf" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="description" content="Hunger Game | Dwarves Foundation" />
-        <meta
-          property="og:description"
-          content="Hunger Game | Dwarves Foundation"
-        />
-        <meta property="og:image" content="/thumbnail.jpeg" />
-        <meta name="twitter:image" content="/thumbnail.jpeg" />
+        <meta name="description" content={metaDesc} />
+        <meta property="og:description" content={metaDesc} />
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta name="twitter:image" content="/thumbnail.png" />
       </Head>
       <GameContextProvider>
         <Component {...pageProps} />
